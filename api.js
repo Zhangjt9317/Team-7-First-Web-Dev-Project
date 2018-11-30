cae3d60f8474eb8322331804f5c8e93261a36762
Jingtian Zhang <jtz9317@gmail.com>
2018-11-29 16:01:10 -0800

    modified map

--- api.js
+++ api.js
@@ -1,22 +1,21 @@
 const config = {
-    apiKey: "AIzaSyCPNtdBaMWSmmCGakJ4GQ2UX6hz4uU2XsQ",
-    authDomain: "idk-you-decide.firebaseapp.com",
-    databaseURL: "https://idk-you-decide.firebaseio.com",
-    projectId: "idk-you-decide",
-    storageBucket: "idk-you-decide.appspot.com",
-    messagingSenderId: "993304386734"
-  };
+  apiKey: "AIzaSyCPNtdBaMWSmmCGakJ4GQ2UX6hz4uU2XsQ",
+  authDomain: "idk-you-decide.firebaseapp.com",
+  databaseURL: "https://idk-you-decide.firebaseio.com",
+  projectId: "idk-you-decide",
+  storageBucket: "idk-you-decide.appspot.com",
+  messagingSenderId: "993304386734"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  const API_KEY_Weather = "090fe3e138a100c59c38bfb81c2de493"
+const API_KEY_Weather = "090fe3e138a100c59c38bfb81c2de493"
 
-
-  // Note remember to create these same variables in Firebase!
+// Note remember to create these same variables in Firebase!
 let itemObject = {
-item: "",
-userlikes,
-userdislikes,
+  item: "",
+  userlikes,
+  userdislikes,
 }
 
 // Click Button changes what is stored in firebase
@@ -24,33 +23,32 @@ $("#like-btn").on("click", function (event) {
 
   // Get inputs
   item = this.textContent
-  itemObject.userlikes+
-  //Change what is saved in firebase
-  database.ref(this).set({
-    userlikes: itemObject.userlikes
-  });
-  });
+  itemObject.userlikes +
+    //Change what is saved in firebase
+    database.ref(this).set({
+      userlikes: itemObject.userlikes
+    });
+});
 
 // Click Button changes what is stored in firebase
 $("#dislike-btn").on("click", function (event) {
 
-    // Get inputs
-    item = this.textContent
-    itemObject.userdislikes+
+  // Get inputs
+  item = this.textContent
+  itemObject.userdislikes +
     //Change what is saved in firebase
     database.ref(this).set({
       userdislikes: itemObject.userdislikes
     });
-    });
+});
 
 //   let numOfOptions = $('#numOfOptionsInput').val().trim();
 //   let activityType = $('#activityTypeInput').val().trim();
 //   let numOfPeople = $('#numOfPeopleInput').val().trim() + "0101";
 //   let gotime = $('#gotimeInput').val().trim() + "0101";
 
-
-  let URL_Weather  = "api.openweathermap.org/data/2.5/forecast";
-  URL_Weather  += '?zip=98105&appid=' + API_KEY_Weather;
+let URL_Weather = "api.openweathermap.org/data/2.5/forecast";
+URL_Weather += '?zip=98105&appid=' + API_KEY_Weather;
 //   '?' + $.param({
 //     // 'id': 524901,
 //     'appid': API_KEY_Weather,
@@ -58,16 +56,10 @@ $("#dislike-btn").on("click", function (event) {
 console.log(URL_Weather)
 
 $.ajax({
-    url: URL_Weather,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response.city.name)
-
-    // for (let i = 0; i < 10; i++) {
-
-
-    // };
-
-  })  .fail(function (err) {
-    throw err;
-  });
+  url: URL_Weather,
+  method: "GET"
+}).then(function (response) {
+  console.log(response.city.name)
+}).fail(function (err) {
+  throw err;
+});
--- location.js
+++ location.js
@@ -1,71 +1,33 @@
  //Google map API used to display google maps and collect info from them 
 
- //  google map API information
+ // google map API information
  var googleAPI_key = "AIzaSyC7vnbVasWSJ-2i10phXjk_Q07yE6tOa-M";
- var mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + googleAPI_key + "&callback=initMap"
+ var mapsURL = "https://maps.googleapis.com/maps/api/js?key=" + googleAPI_key
 
  //global variables should be accessible from other files
- var latitude;
- var longitude;
- var pos;
  var title = "Hello";
- var content;
 
  //clicking on the mapView 
  $("#mapView").click(function initMap() {
+     var map;
+     var infowindow;
+
+     //default setting to be in Seattle
+     pos = {
+         lat: 47.6553216,
+         lng: -122.3155712
+     };
+
      map = new google.maps.Map(document.getElementById('map-container-5'), {
-         center: {
-             lat: 47.605770896274976,
-             lng: -122.33220917514923
-         },
-         zoom: 12,
-         mapTypeId: google.maps.MapTypeId.ROADMAP,
+         center: pos,
+         zoom: 15
      });
 
-     // Try HTML5 geolocation.
-     if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function (position) {
-             pos = {
-                 lat: position.coords.latitude,
-                 lng: position.coords.longitude
-             };
+     map.set('styles', styles);
 
-             var markerImage = 'marker.png';
-             var marker = new google.maps.Marker({
-                 position: pos,
-                 map: map,
-                 icon: markerImage,
-             })
-
-             //event listener for click
-             marker.addListener('click', function () {
-                 infowindow.open(map, marker);
-             });
-
-             //prints out in the div shown by the marker
-             var contentString = '<div class="info-window">' +
-                 '<h3>' + "You are currently here: " + '</h3>' +
-                 '<div class="info-content">' +
-                 '<p>' + content + '</p>' +
-                 '</div>' +
-                 '</div>';
-
-             //creating an infowindow for the 
-             var infowindow = new google.maps.InfoWindow({
-                 content: contentString,
-                 maxWidth: 400
-             });
-
-             map.setCenter(pos);
-         }, function () {
-             handleLocationError(true, infoWindow, map.getCenter());
-         });
-     } else {
-         // Browser doesn't support Geolocation
-         handleLocationError(false, infoWindow, map.getCenter());
-     }
-
+     //styles of google map
      var styles = [{
+
              "elementType": "geometry",
              "stylers": [{
                  "color": "#ebe3cd"
@@ -232,16 +194,94 @@
          }
      ]
 
-     map.set('styles', styles);
+     infowindow = new google.maps.InfoWindow();
+     var service = new google.maps.places.PlacesService(map);
+     service.nearbySearch({
+         location: pos,
+         radius: 500,        //radius can be adjusted 
+         type: ['football'], //type can be changed to search for venues we want 
+         postalcode: 98105,  //zip code can be changed to search for venues we want
 
-     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-         infoWindow.setPosition(pos);
-         infoWindow.setContent(browserHasGeolocation ?
-             'Error: The Geolocation service failed.' :
-             'Error: Your browser doesn\'t support geolocation.');
-         infoWindow.open(map);
+     }, callback);
+
+     //gets the current geolocation
+     if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function (position) {
+             pos = {
+                 lat: position.coords.latitude,
+                 lng: position.coords.longitude
+             };
+
+             var markerImage = 'marker.png';
+             var marker = new google.maps.Marker({
+                 position: pos,
+                 map: map,
+                 icon: markerImage,
+             })
+
+             //event listener for click
+             marker.addListener('click', function () {
+                 infowindow.open(map, marker);
+             });
+
+             //prints out in the div shown by the marker
+             var contentString = '<div class="info-window">' +
+                 '<h3>' + "You are currently here: " + '</h3>' +
+                 '<div class="info-content">' +
+                 '<p>' + "hello and byebye" + '</p>' +
+                 '</div>' +
+                 '</div>';
+
+             //creating an infowindow for the 
+             var infowindow = new google.maps.InfoWindow({
+                 content: contentString,
+                 maxWidth: 400
+             });
+
+             map.setCenter(pos);
+         }, function () {
+             handleLocationError(true, infoWindow, map.getCenter());
+         });
+     } else {
+         // Browser doesn't support Geolocation
+         handleLocationError(false, infoWindow, map.getCenter());
      }
 
-     google.maps.event.addDomListener(window, 'load', initMap);
+     //callback function 
+     function callback(results, status) {
+         if (status === google.maps.places.PlacesServiceStatus.OK) {
+             for (var i = 0; i < results.length; i++) {
+                 createMarker(results[i]);
+             }
+         }
+     }
+
+     //creating marker for places
+     function createMarker(place) {
+         var placeLoc = place.geometry.location;
+         var marker = new google.maps.Marker({
+             map: map,
+             position: place.geometry.location
+         });
+
+         //The Basic category includes the following fields:
+         //address_component, adr_address, alt_id, formatted_address, 
+         //geometry, icon, id, name, permanently_closed, photo, place_id, plus_code, 
+         //scope, type, url, utc_offset, vicinity
+         google.maps.event.addListener(marker, 'click', function () {
+             mainContent = '<div class="info-window">' +
+                 '<h3>' + place.name + '</h3>' +
+                 '<div class="info-content">' +
+                 '<p>' + place.address_component + '</p>' + '<Img src=' + place.icon + '>' +
+                 '<p>' + "type: " + place.types + '<br>' +
+                 '<p>' + "ratings: " + place.rating + '<br>' +
+                 '<p>' + "reviews: " + place.review +
+                 '</div>' +
+                 '</div>';
+
+             infowindow.setContent(mainContent);
+             infowindow.open(map, this);
+         });
+     }
 
  });
